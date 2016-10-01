1669b2b093a8d4c59708982fadc67f87e6c2644f
Richard Harper <dick.harper@gmail.com>
2016-10-01 05:28:10 -0600

    Initial commit

---
+++ CS4250.Prototype.jsTree/Models/DocumentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CS4250.Prototype.jsTree.Models
+{
+    public class DocumentModel
+    {
+        public string Action { get; set; }
+        public string DisplayName { get; set; }
+        public int Id { get; set; }
+        public string ToolTip { get; set; }
+    }
+}
