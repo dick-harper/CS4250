1e2d96a7d33d5a3cf06f0d29c359f78168e7e218
Richard Harper <dick.harper@gmail.com>
2016-10-02 15:18:18 -0600

    Add jstee plugin

--- CS4250.Prototype.jsTree/App_Start/BundleConfig.cs
+++ CS4250.Prototype.jsTree/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace CS4250.Prototype.jsTree
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jstree").Include(
+                      "~/Scripts/jstree.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
